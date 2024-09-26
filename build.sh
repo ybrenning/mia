ed7997c89f765add5279b9d3e06e51a3433db09b
ybrenning <yannickbrenning2@gmail.com>
2024-09-26 20:25:17 +0200

    Add scribe 4

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@ DOCNAME=$1
 DIR="tex/"
 
 pdflatex -output-directory=.. $DIR$DOCNAME.tex
-bibtex -output-directory=.. $DIR$DOCNAME.aux
+bibtex $DIR$DOCNAME.aux
 pdflatex -output-directory=.. $DIR$DOCNAME.tex
 pdflatex -output-directory=.. $DIR$DOCNAME.tex
 
-rm $DIR*.blg $DIR*.bbl $DIR*.aux $DIR*.log $DIR*.out $DIR*.toc
+# rm $DIR*.blg $DIR*.bbl $DIR*.aux $DIR*.log $DIR*.out $DIR*.toc
 rm *.log *.aux
 
 open $DOCNAME.pdf
