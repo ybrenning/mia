1593fdd1d601533d8489ad45ac32db0b77ad5aff
ybrenning <yannickbrenning2@gmail.com>
2024-09-27 16:09:55 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 DOCNAME=$1
 DIR="tex/"
 
-pdflatex -output-directory=.. $DIR$DOCNAME.tex
+pdflatex $DIR$DOCNAME.tex
 bibtex $DIR$DOCNAME.aux
-pdflatex -output-directory=.. $DIR$DOCNAME.tex
-pdflatex -output-directory=.. $DIR$DOCNAME.tex
+pdflatex $DIR$DOCNAME.tex
+pdflatex $DIR$DOCNAME.tex
 
 # rm $DIR*.blg $DIR*.bbl $DIR*.aux $DIR*.log $DIR*.out $DIR*.toc
 rm *.log *.aux
